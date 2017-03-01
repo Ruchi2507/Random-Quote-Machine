9825df9ea605663584de7d448ae25c25a1951f4c
Alex Efremov <alex.efremov.dev@gmail.com>
2017-03-01 21:17:22 +0200

    Use multiple selectors

--- js/index.js
+++ js/index.js
@@ -10,13 +10,10 @@ function getRandomColor() {
 function writeNewQuote()
 {
   var color = getRandomColor();
-  $("body").css("background-color",color);
-  $(".quotation_mark").css("color",color);
-  $(".quote").css("color",color);
-  $(".tweet_quote").css("background-color",color);
-  $(".tweet_quote").css("border-color",color);
-  $(".new_quote").css("background-color",color);
-  $(".new_quote").css("border-color",color);
+  $("body, .tweet_quote, .new_quote").css("background-color", color);
+  $(".quotation_mark, .quote").css("color", color);
+  $(".tweet_quote, .new_quote").css("border-color", color);
+
   $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(a) {
   $(".quote").html("<span class='quoteContent'>" + a["quoteText"]+"</span>");
   $(".quote").append("<p class='title'>— " + a["quoteAuthor"]+ "</p>");
@@ -26,7 +23,7 @@ function writeNewQuote()
 $(document).ready(function()
 {
   writeNewQuote();
-  
+
   $('.post_quote').click(function(){
   var test=$(".quoteContent").html();  $(".post_quote").attr("href",'https://twitter.com/intent/tweet?text='+test);
     console.log('inside')
